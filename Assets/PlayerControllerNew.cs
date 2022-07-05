7d8eb7044d9565f3afa8285f5b94904feb5fbf88
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-07-05 15:20:01 -0300

    Update PlayerControllerNew.cs

--- Assets/PlayerControllerNew.cs
+++ Assets/PlayerControllerNew.cs
@@ -10,6 +10,10 @@ public class PlayerControllerNew : MonoBehaviour
     public Transform visorTR;
     public float visorY;
     public float offsetAngle;
+    public float rotationSpeed;
+    public bool rotateByTracking = false;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +23,17 @@ public class PlayerControllerNew : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Quaternion Angles = InputTracking.GetLocalRotation(XRNode.Head);
-        transform.rotation = Quaternion.Euler(0, Angles.eulerAngles.y * offsetAngle, 0);
+        if (rotateByTracking)
+        {
+            Quaternion Angles = InputTracking.GetLocalRotation(XRNode.Head);
+            transform.rotation = Quaternion.Euler(0, Angles.eulerAngles.y * offsetAngle, 0);
+        }
+        else
+        {
+            transform.Rotate(0f, Input.GetAxis("Horizontal"), 0f);
+        }        
 
-        rb.velocity = Vector3.forward * Input.GetAxis("Vertical") * speed;
+        rb.velocity = transform.forward * Input.GetAxis("Vertical") * speed;
         //transform.localEulerAngles = new Vector3(0f,visorTR.eulerAngles.y,0f);
         //visorY = visorTR.rotation.y;
     }
