89bf96d4dc7edd3e798079edf9782106b6104ebf
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-07-07 14:29:52 -0300

    movement Update

--- Assets/scripts/ContinuosMovement.cs
+++ Assets/scripts/ContinuosMovement.cs
@@ -12,14 +12,16 @@ public class ContinuosMovement : MonoBehaviour
     public LayerMask groundLayer;
     public float additionalHeight = 0.2f;
     public CapsuleCollider Capsule;
+    public float height = .5f;
 
     private float fallingSpeed;
     private XRRig rig;
     private Vector2 inputAxis;
+    private Vector2 inputAxisRotation;
     private CharacterController character;
 
     public bool useVRController = false;
-    public float movementSpeed = 1f;
+    public float movementSpeed = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -39,18 +41,21 @@ public class ContinuosMovement : MonoBehaviour
         else
         {
             inputAxis = new Vector2(Input.GetAxis("Horizontal")*movementSpeed,Input.GetAxis("Vertical")*movementSpeed);
+            inputAxisRotation = new Vector2(Input.GetAxis("rightXboxHorizontal") * movementSpeed, 0);
         }
-        rig.Camera.transform.position = new Vector3(0, rig.Camera.transform.position.y, 0);
     }
 
     private void FixedUpdate()
     {
+        CapsuleFollowHeadSet();
+
         bool isGrounded = CheckIfGrounded();
 
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
         Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
 
         character.Move(direction * Time.fixedDeltaTime * speed);
+        rig.transform.Rotate(0, inputAxisRotation.x, 0);
 
         //Gravity
         if (isGrounded)
@@ -63,13 +68,14 @@ public class ContinuosMovement : MonoBehaviour
             fallingSpeed += gravity * Time.fixedDeltaTime;
         }
         character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
-        CapsuleFollowHeadSet();
+
+        rig.Camera.transform.position = new Vector3(rig.Camera.transform.position.x, height, rig.Camera.transform.position.z);
     }
 
     void CapsuleFollowHeadSet()
     {
-        character.height = 1.8f;
-        Vector3 capsuleCenter = rig.Camera.transform.position;
+        character.height = rig.cameraInRigSpaceHeight - additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
         character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
         Capsule.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
     }
